c9858ab18192aa1cae7b3fa47332cb19a2c56650
cloudZeroToHero <cloudzerotohero@gmail.com>
2022-05-16 11:02:20 +0200

    format correction

--- Code/01-HowToStart/main.tf
+++ Code/01-HowToStart/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">=1.1.8"
-   
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm" #this is shorthand for registry.terraform.io/hashicorp/azurerm
-      version = "~> 3.1.0" #this is the newest version as on 12.04.2022
+      version = "~> 3.1.0"          #this is the newest version as on 12.04.2022
     }
   }
 }
