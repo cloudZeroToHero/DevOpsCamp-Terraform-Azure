9290fdfdcb3d52db2166245e0f348b6cfd3134b1
cloudZeroToHero <cloudzerotohero@gmail.com>
2022-05-17 14:14:14 +0200

    initial commit

---
+++ Code/05-WebServer/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1 align=center> Congratulations </h1>"
+echo "<h1 align=center>! You have just deployed web server :-) </h1>"
+echo "<p align=center> Your server IP is: "$_SERVER['SERVER_ADDR'] "</p>"
+?>
