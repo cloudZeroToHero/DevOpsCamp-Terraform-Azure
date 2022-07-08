df75034aa94fd659163c4e2a4b62f0cb84044883
cloudZeroToHero <cloudzerotohero@gmail.com>
2022-07-08 12:36:24 +0200

    Name convention changed

---
+++ Code/07-CAFNaming/variables.tf
@@ -0,0 +1,45 @@
+
+variable "project-name" {
+  type    = string
+  default = "DevOpsCamp"
+}
+
+variable "environment" {
+  type    = string
+  default = "Dev"
+  validation {
+    condition     = contains(["Dev", "Prod"], var.environment)
+    error_message = "Possible values for environment are \"Dev\" or \"Prod\"."
+  }
+}
+
+variable "ssh-source-addresss"{ # I can put my IP address there to mitigate vulnerability 
+  type = string
+  default = "*"
+}
+
+
+variable "az-region" {
+  type = map
+  default = {
+    "Dev"  = "eastus"
+    "Prod" = "westus"
+  }
+}
+
+
+variable "vnet-addr" {
+  type = map
+  default = {
+    "Dev"  = ["10.10.0.0/16"]
+    "Prod" = ["10.20.0.0/16"]
+  }
+}
+
+variable "subnet" {
+  type = map
+  default = {
+    "Dev"  = ["10.10.1.0/24"]
+    "Prod" = ["10.20.2.0/24"]
+  }
+}
